55232bd37a02f3eac418889fbcc17de630f2408d
Marina Gavilan <maxi_de_estacion013@hotmail.com>
2022-11-05 01:46:23 -0300

    arreglos

--- src/4_footer/contacts.js
+++ src/4_footer/contacts.js
@@ -24,7 +24,7 @@ function Contacts() {
                         <ul>
                             <li>Tel: <br/> 351-8109801</li>
                             <li>Mail: <br/> maxiperez283@gmail.com</li>
-                            <li>linkedin: <br/>www.linkedin.com/in/maxi-perez-3a0953228</li>                     
+                            <li>linkedin: <br/><a href='https://www.linkedin.com/in/maxi-perez-3a0953228' target={'_blank'}>www.linkedin.com/in/maxi-perez-3a0953228</a></li>                     
                         </ul>
                     </Card>
                 </div>

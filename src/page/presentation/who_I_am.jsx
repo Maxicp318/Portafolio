4b1644697c6673720de6fbd66062f90cd9f0907a
“Maxi Perez <maxi_de_estacion013@hotmail.com>
2024-03-05 19:11:38 -0300

    modif presentacion

--- src/page/presentation/who_I_am.jsx
+++ src/page/presentation/who_I_am.jsx
@@ -8,16 +8,19 @@ function Presentation() {
         <div className={styles.containerPresentation} id='who_i_am'>  
             <Btn addStyle={styles.btnDescarga} props={'Descargar Cv'} link={'https://drive.google.com/file/d/1FGIrCaZSOscZpCJo1ogAGVGxRgK34BNx/view?usp=sharing'} pagina={"_blank"}/>    
             <img className={styles.perfil} src={Foto_Perfil_2} alt="foto_perfil"/>      
-            <h1 className='animate__animated animate__backInDown'>Hola, soy Maximiliano Perez</h1>
+            <h1 className={`animate__animated animate__backInDown ${styles.titulos}`}>Hola, soy Maximiliano Perez</h1>
+            <h2 className={`animate__animated animate__backInDown ${styles.titulos}`}>Tecnico Superior En Programacion</h2>
             <article className={styles.containerText}>
-                <p className={`animate__animated animate__fadeInDown who_I_am ${styles.textPresentation}`}>Un estudiante de la carrera Tecnico Superior En
-                    Programacion en el Instituto Superior Teclab.
-                    <br />Me describo como una persona comprometida, responsable y muy apasionada en el aprendisaje;
-                    <br /> Cuento con gran motivación para avanzar en mi carrera
-                    profesional y seguir adquiriendo nuevos conocimientos dia a dia siendo autodidacta.<br/>
-                    Me destaco por trabajar en equipo y poder adaptarme muy bien al cambios. <br/>
-                    Mi objetivo es encontrar una empresa que me permita aplicar mis conocimientos o
-                    adquirir nuevos que sirvan para la productividad de la misma.
+                <p className={`animate__animated animate__fadeInDown who_I_am ${styles.textPresentation}`}>
+                    Recibido del Instituto Superior Teclab.
+                    <br />Me describo como una persona comprometida, proactivo y siempre con actitud positiva.
+                    <br />Me destaco por mi empatia para formar buenas relaciones personales
+                    y adaptarme muy bien al trabajar en equipo.
+                    <br/>Cuento con muchos conocimientos y estoy abierto ha aprender nuevas tecnologias 
+                    e habilidades que me complementen para seguir creciendo en mi desarrollo profesional
+                    <br />
+                    Mi objetivo es entrar a la industria IT; 
+                    sumar experiencia para lograr ser un gran profesional en el mundo de la Proramacion. 
                 </p>
             </article>
         </div>

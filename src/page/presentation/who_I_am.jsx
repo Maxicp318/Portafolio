6ae12e45ccd33833e0e13b7e07262c1b8b7940ea
Carlos Maximiliano Perez <87154912+Maxicp318@users.noreply.github.com>
2024-03-05 21:24:30 -0300

    Update who_I_am.jsx
    Corrección de errores de escritura

--- src/page/presentation/who_I_am.jsx
+++ src/page/presentation/who_I_am.jsx
@@ -9,18 +9,18 @@ function Presentation() {
             <Btn addStyle={styles.btnDescarga} props={'Descargar Cv'} link={'https://drive.google.com/file/d/1FGIrCaZSOscZpCJo1ogAGVGxRgK34BNx/view?usp=sharing'} pagina={"_blank"}/>    
             <img className={styles.perfil} src={Foto_Perfil_2} alt="foto_perfil"/>      
             <h1 className={`animate__animated animate__backInDown ${styles.titulos}`}>Hola, soy Maximiliano Perez</h1>
-            <h2 className={`animate__animated animate__backInDown ${styles.titulos}`}>Tecnico Superior En Programacion</h2>
+            <h2 className={`animate__animated animate__backInDown ${styles.titulos}`}>Técnico Superior En Programación</h2>
             <article className={styles.containerText}>
                 <p className={`animate__animated animate__fadeInDown who_I_am ${styles.textPresentation}`}>
                     Recibido del Instituto Superior Teclab.
                     <br />Me describo como una persona comprometida, proactivo y siempre con actitud positiva.
-                    <br />Me destaco por mi empatia para formar buenas relaciones personales
+                    <br />Me destacó por mi empatia para formar buenas relaciones personales
                     y adaptarme muy bien al trabajar en equipo.
                     <br/>Cuento con muchos conocimientos y estoy abierto ha aprender nuevas tecnologias 
                     e habilidades que me complementen para seguir creciendo en mi desarrollo profesional
                     <br />
                     Mi objetivo es entrar a la industria IT; 
-                    sumar experiencia para lograr ser un gran profesional en el mundo de la Proramacion. 
+                    sumar experiencia para lograr ser un gran profesional en el mundo de la Programación. 
                 </p>
             </article>
         </div>
